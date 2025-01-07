e4a83039fe5ed8ccbeaa8ec49a8612fd3d6761fd
Robert <r.lapins@proton.me>
2025-01-07 01:15:58 +0100

    grande finale

--- creditcalc.py
+++ creditcalc.py
@@ -1,16 +1,15 @@
 import argparse
 from math import ceil, log
+from sys import exit
 
 class LoanCalc:
     """Class for calculating monthly loan repayments."""
-    def __init__(self, interest, principal=None, payment=None, periods=None):
+    def __init__(self, type, interest, principal=None, payment=None, periods=None):
+        self.type = type
         self.principal = principal
         self.payment = payment
         self.interest = interest / 12 / 100
         self.periods = periods
-
-    def print(self):
-        return f"{self.principal, self.payment, self.interest, self.periods}"
 
     def months_of_repayment(self):
         """Calculates the number of months needed to repay the loan."""
@@ -21,10 +20,11 @@ class LoanCalc:
 
         x = (A / (A - i * P))
         base = 1 + i
-        self.periods = ceil(log(x, base))
+        n = ceil(log(x, base))
+        self.periods = n
 
-        years = self.periods // 12
-        months = self.periods % 12
+        years = n // 12
+        months = n % 12
         answer = ""
 
         if years:
@@ -37,62 +37,89 @@ class LoanCalc:
             answer += f"{months} month"
             if months > 1:
                 answer += "s"
-        return f"It will take {answer} to repay this loan!"
+        return f"It will take {answer} to repay this loan!\nOverpayment = {int(A * n - P)}"
 
     def monthly_payment(self):
         """Calculates the monthly payment (the annuity payment)."""
         P, n, i = self.principal, self.periods, self.interest
 
         f = (1 + i)**n
-        A = P * i * f / (f - 1)
-        self.payment = ceil(A)
-        return f"Your monthly payment = {self.payment}!"
+        A = ceil(P * i * f / (f - 1))
+        self.payment = A
+        return f"Your monthly payment = {A}!\nOverpayment = {int(A * n - P)}"
 
     def loan_principal(self):
         """Calculates the loan principal."""
         A, n, i = self.payment, self.periods, self.interest
 
         f = (1 + i)**n
-        P = A * (f - 1) / (i * f)
-        return f"Your loan principal = {round(P)}!"
+        P = ceil(A * (f - 1) / (i * f))
+        self.principal = P
+        return f"Your loan principal = {P}!\nOverpayment = {int(A * n - P)}"
 
-def are_they_positive(args):
-    """Verifies that the values of the arguments are positive. If not, retrieves the correct value from the user."""
-    for key, value in vars(args).items():
-        if value is None:
-            continue
-        while value <= 0:
-            try:
-                if value > 0:
-                    break
-                value = float(input(f"Please enter a positive number for \"--{key}\" (current value: {value}): "))
-                if value <= 0:
-                    raise ValueError
-                setattr(args, key, value)
-            except ValueError:
-                print(f"Invalid input. \"--{key}\" must be a positive number.")
-    return args
+    def diff_payment(self):
+        """Calculates the monthly payments (the differential payment)."""
+        P, n, i = self.principal, self.periods, self.interest
+        total = 0
+        for m in range(n):
+            m += 1
+            D_m = ceil(P / n + i * (P - P * (m - 1) / n))
+            total += D_m
+            print(f"Month {m}: payment is {D_m}")
+        return f"\nOverpayment = {int(total - P)}"
+
+# def are_params_correct(args):
+    # """Verifies that the values of the arguments are positive. If not, retrieves the correct value from the user."""
+    # for key, value in vars(args).items():
+    #     if value is None or type(value) is str:
+    #         continue
+    #     while value <= 0:
+    #         try:
+    #             if value > 0:
+    #                 break
+    #             value = float(input(f"Please enter a positive number for \"--{key}\" (current value: {value}): "))
+    #             if value <= 0:
+    #                 raise ValueError
+    #             setattr(args, key, value)
+    #         except ValueError:
+    #             print(f"Invalid input. \"--{key}\" must be a positive number.")
+    # return args
 
 def main():
     parser = argparse.ArgumentParser(description="This program calculates the number of monthly payments, the monthly payment amount, and the loan principal depending on what is missing from the input data.")
+    parser.add_argument("--type", type=str, help="You need to choose only one type from the list.")
     parser.add_argument("--principal", type=float, help="You can get its value if you know the interest, annuity payment, and number of months.")
     parser.add_argument("--payment", type=float, help="The payment amount. It can be calculated using the provided principal, interest, and number of months.")
     parser.add_argument("--periods", type=int, help="Denotes the number of months needed to repay the loan. It's calculated based on the interest, annuity payment, and principal.")
-    parser.add_argument("--interest", required=True, type=float, help="Specified without a percent sign. It must always be provided.")
+    parser.add_argument("--interest", type=float, help="Specified without a percent sign. It must always be provided.")
 
+    # args = parser.parse_args(['--type', 'diff', '--principal', '1000000', '--periods', '10', '--interest', '10'])
     args = parser.parse_args()
-    # args = parser.parse_args(['--principal', '-1000000', '--periods', '0', '--interest', '-10'])
-    are_they_positive(args)
+    t, p, a, n, i = args.type, args.principal, args.payment, args.periods, args.interest
 
-    calculator = LoanCalc(args.interest, args.principal, args.payment, args.periods)
-    if args.periods is None and args.payment and args.principal:
-        print(calculator.months_of_repayment())
-    elif args.payment is None and args.principal and args.periods:
-        print(calculator.monthly_payment())
-    elif args.principal is None and args.payment and args.periods:
-        print(calculator.loan_principal())
+    # Checks the correctness of the parameters
+    if None in [t, i]:
+        print("Incorrect parameters")
+        exit()
+    elif any([x is not None and x < 0 for x in [p, a, n, i]]):
+        print("Incorrect parameters")
+        exit()
+
+    calculator = LoanCalc(t, i, p, a, n)
+
+    if t == "annuity":
+        if all([not p, a, n]):
+            print(calculator.loan_principal())
+        elif all([p, not a, n]):
+            print(calculator.monthly_payment())
+        elif all([p, a, not n]):
+            print(calculator.months_of_repayment())
+        else:
+            print("Incorrect parameters")
+    elif t == "diff" and all([p, n]):
+        print(calculator.diff_payment())
     else:
-        print("Insert also a value for a missing one out of these: \"--principal\", \"--payment\", \"--periods\", \"--interest\"!!!")
+        print("Incorrect parameters")
 
 if __name__ == '__main__':
     main()
